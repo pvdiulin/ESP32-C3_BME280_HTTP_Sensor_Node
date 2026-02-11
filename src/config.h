24d534de63e64d0d0e85790be453e40b132cd91b
madhattermonroe <pdiulin@yandex.ru>
2026-02-11 19:14:24 +0300

    Updating code and README.MD

--- src/config.h
+++ src/config.h
@@ -1,11 +1,12 @@
 #pragma once
 
 // WiFi
-#define WIFI_SSID     "X"
-#define WIFI_PASSWORD "X"
+#define WIFI_SSID     ""
+#define WIFI_PASSWORD ""
 
 // Endpoint
 #define API_ENDPOINT  "http://192.168.0.1:8080/thmetrix/test"
 
 // Behaviour
 #define SEND_INTERVAL 5000
+#define LOCATION      ""
--- src/main.cpp
+++ src/main.cpp
@@ -2,55 +2,66 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
 #include <ArduinoJson.h>
+#include <Wire.h>
+#include <Adafruit_BME280.h>
 #include "config.h"
+
+Adafruit_BME280 bme;
 
 void connectWiFi() {
 
   WiFi.mode(WIFI_STA);
   WiFi.setSleep(false);
-
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 
-  Serial.print("Connecting");
+  Serial.print("Connecting to WiFi");
 
-  while (WiFi.status() != WL_CONNECTED) {
+  unsigned long start = millis();
+
+  while (WiFi.status() != WL_CONNECTED && millis() - start < 15000) {
     delay(500);
     Serial.print(".");
   }
 
-  Serial.println("\nConnected!");
-
-  WiFi.config(
-    INADDR_NONE,
-    INADDR_NONE,
-    INADDR_NONE,
-    IPAddress(8, 8, 8, 8),
-    IPAddress(1, 1, 1, 1)
-  );
-
-  Serial.println(WiFi.localIP());
+  if (WiFi.status() == WL_CONNECTED) {
+    Serial.println("\nConnected!");
+    Serial.print("IP: ");
+    Serial.println(WiFi.localIP());
+  } else {
+    Serial.println("\nWiFi connection timeout.");
+  }
 }
 
 void sendJson() {
 
   if (WiFi.status() != WL_CONNECTED) {
-    Serial.println("WiFi reconnect...");
-    WiFi.reconnect();
-    delay(1000);
+    Serial.println("WiFi lost. Reconnecting...");
+    connectWiFi();
     return;
   }
 
-  StaticJsonDocument<200> doc;
-  doc["device"] = "esp32-c3";
-  doc["uptime"] = millis();
-  doc["freeHeap"] = ESP.getFreeHeap();
+  float temp = bme.readTemperature();
+  float hum  = bme.readHumidity();
+  float pres = bme.readPressure() / 100.0;
+
+  if (isnan(temp) || isnan(hum) || isnan(pres)) {
+    Serial.println("Sensor read error!");
+    return;
+  }
+
+  StaticJsonDocument<256> doc;
+
+  doc["location"] = LOCATION;
+  doc["temperature"] = temp;
+  doc["humidity"] = hum;
+  doc["pressure"] = pres;
 
   String payload;
   serializeJson(doc, payload);
 
   for (int attempt = 1; attempt <= 3; attempt++) {
 
-    Serial.printf("\nAttempt %d\n", attempt);
+    Serial.printf("\nHTTP attempt %d\n", attempt);
 
     HTTPClient http;
     http.setTimeout(4000);
@@ -81,13 +92,27 @@ void sendJson() {
 }
 
 void setup() {
+
   Serial.begin(115200);
   delay(1000);
+
+  Serial.println("\nESP32-C3 BME280 sender starting...");
+
+  Wire.begin(8, 9, 400000);
+
+  if (!bme.begin(0x76)) {
+    Serial.println("BME280 not found!");
+    while (true);
+  }
+
+  Serial.println("BME280 initialized.");
 
   connectWiFi();
 }
 
 void loop() {
+
   sendJson();
+
   delay(SEND_INTERVAL);
 }
